fe748044e09979a55072c8fdbc75bfc8ddb59db5
imamsudarajat04 <imamsudarajat708@gmail.com>
2025-02-02 16:27:37 +0700

    fix error css and js in template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -93,9 +93,9 @@
 
     <script src="{{ asset('/template/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('/template/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('/tempate/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('/template/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset('/template/js/ruang-admin.min.js') }}"></script>
-    <script src="{{ asset('/templte/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('/template/js/demo/chart-area-demo.js') }}"></script>
 
 
     @stack('scripts')
